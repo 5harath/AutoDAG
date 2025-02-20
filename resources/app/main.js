e776e3b054aa7bd530c461aef1d4328b5cd6df9d
5harath <shartja@gmail.com>
2025-02-20 15:13:53 +0530

    Second Commit

--- resources/app/main.js
+++ resources/app/main.js
@@ -5,6 +5,8 @@ const {app, BrowserWindow} = require('electron')
 
 let mainWindow
 
+window.$ = window.jQuery = require(".js/loadScript.js");
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
